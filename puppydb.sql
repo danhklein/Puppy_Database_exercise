0297bc2ccd0e95952ca93b63a178d261cbcb26ec
Daniel Klein <danhirschklein@gmail.com>
2016-02-25 14:08:04 -0700

    Added Ralph

--- puppydb.sql
+++ puppydb.sql
@@ -7,9 +7,12 @@ CREATE DATABASE puppies;
 DROP TABLE IF EXISTS dogs;
 
 CREATE TABLE dogs (
+  id serial PRIMARY KEY;
   name VARCHAR,
   breed VARCHAR,
   AGE INTEGER,
   SEX VARCHAR,
   ALIVE BOOLEAN
 );
+
+INSERT INTO dogs (name, breed, age, sex, alive) VALUES ('Ralph', 'Westie', 14, 'M', false)l
